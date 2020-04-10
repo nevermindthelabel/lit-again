7cff1f214cdd959508fbb67b844f642094fd3e81
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-09 21:33:25 -0700

    added map method

--- todo-app.js
+++ todo-app.js
@@ -2,7 +2,7 @@ import { LitElement, html } from 'https://unpkg.com/lit-element?module';
 
 const author = 'open-wc.org';
 const homepage = 'https://open-wc.org';
-const footerTemplate = html `<footer>Made with 💗 by <a href="${homepage}">${author}</a></footer>`;
+const footerTemplate = html`<footer>Made with 💗 by <a href="${homepage}">${author}</a></footer>`;
 
 class TodoApp extends LitElement {
   constructor() {
@@ -15,8 +15,14 @@ class TodoApp extends LitElement {
   }
 
   render() {
-    return html `
+    return html`
     <h1>Todo app with Lit Element and Lit HTML</h1>
+
+    <ol>
+      ${this.todos.map(todo => html`
+        <li>${todo}</li>
+      `)}
+    </ol>
     ${this.todos}
     ${footerTemplate}
     `
