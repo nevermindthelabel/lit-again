f51cc2bde66353de0e2fb113e8c4c4503cab722e
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-09 21:18:42 -0700

    moved html to render method

--- todo-app.js
+++ todo-app.js
@@ -1,9 +1,15 @@
-import { LitElement } from 'https://unpkg.com/lit-element?module';
+import { LitElement, html } from 'https://unpkg.com/lit-element?module';
 
 class TodoApp extends LitElement {
   connectedCallback() {
     super.connectedCallback();
     console.log('Lit Element works');
+  }
+
+  render() {
+    return html `
+    <h1>Todo app with Lit Element and Lit HTML</h1>
+    `
   }
 }
 
