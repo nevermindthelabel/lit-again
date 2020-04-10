bc5809a49dee7f1707080a20cdcc63a6a20f89dd
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-09 21:15:27 -0700

    connected JS to DOM

--- todo-app.js
+++ todo-app.js
@@ -1,2 +1,10 @@
 import { LitElement } from 'https://unpkg.com/lit-element?module';
 
+class TodoApp extends LitElement {
+  connectedCallback() {
+    super.connectedCallback();
+    console.log('Lit Element works');
+  }
+}
+
+customElements.define('todo-app', TodoApp);
