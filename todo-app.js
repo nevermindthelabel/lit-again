05c4894fe36178fc606b28d40fdc3914706107e1
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-09 21:26:31 -0700

    added variables

--- todo-app.js
+++ todo-app.js
@@ -1,5 +1,8 @@
 import { LitElement, html } from 'https://unpkg.com/lit-element?module';
 
+const author = 'open-wc.org';
+const footerTemplate = html `<footer>Made with 💗 by <a href="https://open-wc.org">${author}</a></footer>`;
+
 class TodoApp extends LitElement {
   connectedCallback() {
     super.connectedCallback();
@@ -9,6 +12,7 @@ class TodoApp extends LitElement {
   render() {
     return html `
     <h1>Todo app with Lit Element and Lit HTML</h1>
+    ${footerTemplate}
     `
   }
 }
