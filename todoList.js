c9b5a292f65f8fe9e3b32ebad859387f1f273baa
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-10 10:21:02 -0700

    added todo list logic

--- todoList.js
+++ todoList.js
@@ -1,7 +1,38 @@
 import { html, LitElement } from 'https://unpkg.com/lit-element?module';
 
 class TodoList extends LitElement {
-
+  static get properties() {
+    return {
+      todos: { type: Array }
+    }
+  }
+  render() {
+    if (!this.todos) {
+      return html``;
+    }
+    return html`
+      <ol>
+        ${this.todos.map(todo => html`
+          <li>
+            <input
+              type="checkbox"
+              .checked="${todo.finished}"
+              @change="${e => this._changeTodoFinished(e, todo)}"
+            />
+            ${todo.text}
+            <button @click="${() => this._removeTodo(todo)}">❌</button>
+          </li>
+        `)}
+      </ol>
+    `;
+  }
+  _changeTodoFinished(e, updatedTodo) {
+    const eventDetails = { updatedTodo, finished: e.target.checked};
+    this.dispatchEvent(new CustomEvent('change-todo-finished', { detail: eventDetails }));
+  }
+  _removeTodo(item) {
+    this.dispatchEvent(new CustomEvent('remove-todo', { detail: item }));
+  }
 }
 
 customElements.define('todo-list', TodoList);
